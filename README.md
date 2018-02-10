d3f8ba30953fcb02eea39b972e2233f3e2db037f
M@nojrpms <30459831+VV-MANOJ@users.noreply.github.com>
2018-02-11 02:34:10 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Movie-Website-Project-Python
+# Project 1: Movie Trailer Website
+### by Vardhineni Venkata Manoj
+
+
+## Requirement
+
+A Python program that produces the HTML for a movie website that displays
+a number of movies. Click on a movie poster to play its trailer.
+
+## Required Libraries and Dependencies
+
+Python 2.x is required to run this project. The Python executable should be in
+your default path, which the Python installer should have set.
+
+## Project contents
+
+This project consists for the following files:
+
+* entertainment_center.py - main Python script to run
+* media.py - contains the class Movie that stores movie details
+* fresh_tomatoes.py - creates the HTML file for the website (Udacity supplied)
+
+## How to Run Project
+
+Download the project zip file to you computer and unzip the file. Or clone this
+repository to your desktop.
+
+Open the text-based interface for your operating system (e.g. the terminal
+window in Linux, the command prompt in Windows).
+
+Navigate to the project directory and type in the following command:
+
+```bash
+python entertainment_center.py
+```
+
+Your default browser should launch a new tab displaying the movie trailer website.
+
+Resources
+https://discussions.udacity.com/c/nd000-stage-3-create-a-movie-website
+http://www.w3schools.com/html/html_css.asp
+https://docs.python.org/2/reference/datamodel.html
+https://www.python.org/dev/peps/pep-0257/#multi-line-docstrings
+http://pep8online.com/
+
---
+++ entertainment_center.py
@@ -0,0 +1,81 @@
+#this is main file where we create the instances of Movie class
+#and run the file to view the movie website page
+# we have to import media and fresh_tomatoes python files
+import media
+import fresh_tomatoes
+
+#below are various instances (alaska,bfg,starwar,american_sniper,
+#now_you_can_see_me,the_secret_life_of_pets) of movie class
+
+
+
+thor = media.Movie("Thor Ragnarok",
+                        "150 minutes",
+                        "180p",
+                        "Ragnarok is a 2017 American superhero film based on the Marvel Comics character Thor",
+                        "Action",
+                        "Manoj",
+                        "https://cdn.images.express.co.uk/img/dynamic/36/590x/thor-ragnarok-end-credits-avengers-infinity-war-870508.jpg",
+                        "https://www.youtube.com/watch?v=ue80QwXMRHg",
+                        "R")
+
+wonder_woman = media.Movie("Wonder Women",
+                            "150 minutes",
+                            "180p",
+                            "Wonder Woman is a 2017 American superhero film based on the DC Comics character of the same name, distributed by Warner Bros.",
+                            "Action",
+                            "Manoj",
+                            "http://www.atoupeira.com.br/wp-content/uploads/2017/09/Capa_Mulher-Maravilha_Looke.jpg",
+                            "https://www.youtube.com/watch?v=VSB4wGIdDwo",
+                            "RG")
+
+logan = media.Movie("Logan",
+                    "150 minutes",
+                    "180p",
+                    "Logan is a 2017 American superhero film, produced by Marvel Entertainment",
+                    "Action",
+                    "Manoj",
+                    "https://upload.wikimedia.org/wikipedia/en/3/37/Logan_2017_poster.jpg",
+                    "https://www.youtube.com/watch?v=Div0iP65aZo",
+                    "U/Z")
+
+justice_league = media.Movie("Justice League",
+	                        "150 minutes",
+                    		"180p",
+                            "Justice League is a 2017 American superhero film based on the DC Comics ",
+                        	"Action",
+                            "Manoj",
+                        	"https://upload.wikimedia.org/wikipedia/en/3/31/Justice_League_film_poster.jpg",
+                        	"https://www.youtube.com/watch?v=3cxixDgHUYw",
+                        	"F")
+jumanji = media.Movie("Jumanji - Welcome to the Jungle",
+						"150 minutes",
+                    	"180p",
+                    	"Jumanji: Welcome to the Jungle is a 2017 American comedy ",
+                        "Action",
+                        "Manoj",
+                        "https://upload.wikimedia.org/wikipedia/en/d/dc/Jumanji_Welcome_to_the_Jungle.png",
+                        "https://www.youtube.com/watch?v=2QKg5SZ_35I",
+                        "E")
+
+baby_driver = media.Movie("Baby Driver",
+						"150 minutes",
+                        "180p",
+                        "Baby Driver is a 2017 action crime film written and directed by Edgar Wright",
+                        "Action",
+                        "Manoj",
+                        "https://upload.wikimedia.org/wikipedia/en/8/8e/Baby_Driver_poster.jpg",
+                        "https://www.youtube.com/watch?v=z2z857RSfhk",
+                        "E")
+
+
+# we create a list of instances of movie class and assign it to movies variable
+movies = [thor,wonder_woman,logan,justice_league,jumanji,baby_driver]
+
+# calling open_movies_page method to view movie website
+# input is  list of all the instances
+fresh_tomatoes.open_movies_page(movies)
+
+# print media.Movie.show_image.__doc__
+#(above line is one example how to read the documentation of
+#function of Movie class)
